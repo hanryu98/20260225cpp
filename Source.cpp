d7ac50f14336eca4d4463d40a37f2cecfa88cd80
hanryu98 <hanryu98@users.noreply.github.com>
2026-02-25 09:16:06 +0900

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int NumberOne;
+	int NumberTwo;
+
+	cin >> NumberOne >> NumberTwo;
+
+	cout << NumberOne + NumberTwo << endl;
+	cout << NumberOne - NumberTwo << endl;
+	cout << NumberOne * NumberTwo << endl;
+	cout << NumberOne / NumberTwo << endl;
+	cout << NumberOne % NumberTwo << endl;
+
+
+}
