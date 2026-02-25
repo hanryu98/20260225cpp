0e1783952c5d353ed313f817fc2c1ce4114729d2
hanryu98 <hanryu98@users.noreply.github.com>
2026-02-25 09:18:50 +0900

    Code
    수정

--- Source.cpp
+++ Source.cpp
@@ -14,6 +14,6 @@ int main()
 	cout << NumberOne * NumberTwo << endl;
 	cout << NumberOne / NumberTwo << endl;
 	cout << NumberOne % NumberTwo << endl;
-
+	cout << NumberOne + NumberTwo << endl;
 
 }
